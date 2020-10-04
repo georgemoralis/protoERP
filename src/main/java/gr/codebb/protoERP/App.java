ae171fa021301aaf1396e75775ce6d2d3c046dff
georgemoralis <giorgosmrls@gmail.com>
2020-10-04 17:12:23 +0300

    πρώτο commit. Βασική δομή

---
+++ src/main/java/gr/codebb/protoERP/App.java
@@ -0,0 +1,21 @@
+package gr.codebb.protoERP;
+
+import static javafx.application.Application.launch;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+  @Override
+  public void init() throws Exception {
+    super.init();
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {}
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module codebb.protoERP {
+  requires transitive javafx.controls;
+  requires transitive javafx.fxml;
+  requires java.base;
+  requires java.desktop;
+  requires lombok;
+
+  opens gr.codebb.protoERP to javafx.fxml;
+
+  exports gr.codebb.protoERP;
+}
