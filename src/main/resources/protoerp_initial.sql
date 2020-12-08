34194788be508df3327ab563d1b2d29fb00ae952
georgemoralis <giorgosmrls@gmail.com>
2020-12-08 14:23:49 +0200

    προσθήκης database με δικαιώματα χρηστών

--- src/main/resources/protoerp_initial.sql
+++ src/main/resources/protoerp_initial.sql
@@ -1,5 +1,103 @@
 --
--- Table structure for table `settings`
+-- Δομή πίνακα για τον πίνακα `databasechangelog`
+--
+
+CREATE TABLE `databasechangelog` (
+  `ID` varchar(255) NOT NULL,
+  `AUTHOR` varchar(255) NOT NULL,
+  `FILENAME` varchar(255) NOT NULL,
+  `DATEEXECUTED` datetime NOT NULL,
+  `ORDEREXECUTED` int(11) NOT NULL,
+  `EXECTYPE` varchar(10) NOT NULL,
+  `MD5SUM` varchar(35) DEFAULT NULL,
+  `DESCRIPTION` varchar(255) DEFAULT NULL,
+  `COMMENTS` varchar(255) DEFAULT NULL,
+  `TAG` varchar(255) DEFAULT NULL,
+  `LIQUIBASE` varchar(20) DEFAULT NULL,
+  `CONTEXTS` varchar(255) DEFAULT NULL,
+  `LABELS` varchar(255) DEFAULT NULL,
+  `DEPLOYMENT_ID` varchar(10) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `databasechangeloglock`
+--
+
+CREATE TABLE `databasechangeloglock` (
+  `ID` int(11) NOT NULL,
+  `LOCKED` bit(1) NOT NULL,
+  `LOCKGRANTED` datetime DEFAULT NULL,
+  `LOCKEDBY` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `databasechangeloglock`
+--
+
+INSERT INTO `databasechangeloglock` (`ID`, `LOCKED`, `LOCKGRANTED`, `LOCKEDBY`) VALUES
+(1, b'0', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` bigint(20) NOT NULL,
+  `permissionDisplayName` varchar(255) DEFAULT NULL,
+  `permissionName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `permissionDisplayName`, `permissionName`) VALUES
+(1, 'Διαχείριση χρηστών', 'USER_MANAGEMENT');
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `roles`
+--
+
+CREATE TABLE `roles` (
+  `id` bigint(20) NOT NULL,
+  `roleName` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `roles`
+--
+
+INSERT INTO `roles` (`id`, `roleName`) VALUES
+(1, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `role_permission`
+--
+
+CREATE TABLE `role_permission` (
+  `role_id` bigint(20) NOT NULL,
+  `permission_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `role_permission`
+--
+
+INSERT INTO `role_permission` (`role_id`, `permission_id`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `settings`
 --
 
 CREATE TABLE `settings` (
@@ -10,29 +108,142 @@ CREATE TABLE `settings` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `settings`
+-- Άδειασμα δεδομένων του πίνακα `settings`
 --
 
 INSERT INTO `settings` (`id`, `companyId`, `settingName`, `settingValue`) VALUES
-(1, -1, 'checkUpdates', '1');
+(1, -1, 'product_version', '0.0.114');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Δομή πίνακα για τον πίνακα `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) NOT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `users`
+--
+
+INSERT INTO `users` (`id`, `active`, `name`, `password`, `username`) VALUES
+(1, b'1', 'admin', '', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Δομή πίνακα για τον πίνακα `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Άδειασμα δεδομένων του πίνακα `user_role`
+--
+
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
+(1, 1);
+
+--
+-- Ευρετήρια για άχρηστους πίνακες
 --
 
 --
--- Indexes for table `settings`
+-- Ευρετήρια για πίνακα `databasechangeloglock`
+--
+ALTER TABLE `databasechangeloglock`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Ευρετήρια για πίνακα `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Ευρετήρια για πίνακα `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Ευρετήρια για πίνακα `role_permission`
+--
+ALTER TABLE `role_permission`
+  ADD KEY `FK2xn8qv4vw30i04xdxrpvn3bdi` (`permission_id`),
+  ADD KEY `FKtfgq8q9blrp0pt1pvggyli3v9` (`role_id`);
+
+--
+-- Ευρετήρια για πίνακα `settings`
 --
 ALTER TABLE `settings`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Ευρετήρια για πίνακα `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Ευρετήρια για πίνακα `user_role`
+--
+ALTER TABLE `user_role`
+  ADD KEY `FKt7e7djp752sqn6w22i6ocqy6q` (`role_id`),
+  ADD KEY `FKj345gk1bovqvfame88rcx7yyx` (`user_id`);
+
+--
+-- AUTO_INCREMENT για άχρηστους πίνακες
 --
 
 --
--- AUTO_INCREMENT for table `settings`
+-- AUTO_INCREMENT για πίνακα `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT για πίνακα `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT για πίνακα `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT για πίνακα `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Περιορισμοί για άχρηστους πίνακες
+--
+
+--
+-- Περιορισμοί για πίνακα `role_permission`
+--
+ALTER TABLE `role_permission`
+  ADD CONSTRAINT `FK2xn8qv4vw30i04xdxrpvn3bdi` FOREIGN KEY (`permission_id`) REFERENCES `permissions` (`id`),
+  ADD CONSTRAINT `FKtfgq8q9blrp0pt1pvggyli3v9` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
+
+--
+-- Περιορισμοί για πίνακα `user_role`
+--
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `FKj345gk1bovqvfame88rcx7yyx` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `FKt7e7djp752sqn6w22i6ocqy6q` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
 COMMIT;
