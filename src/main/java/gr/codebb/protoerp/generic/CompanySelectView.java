0199675ae175d1a684067cbfd211af9a78d5ccc2
georgemoralis <giorgosmrls@gmail.com>
2021-02-26 14:40:55 +0200

    CompanySelectView: disable buttons if no selection

--- src/main/java/gr/codebb/protoerp/generic/CompanySelectView.java
+++ src/main/java/gr/codebb/protoerp/generic/CompanySelectView.java
@@ -12,6 +12,7 @@ import gr.codebb.protoerp.settings.company.CompanyQueries;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
@@ -32,115 +33,107 @@ import org.controlsfx.control.MaskerPane;
 
 public class CompanySelectView implements Initializable {
 
-    @FXML
-    private TableView<CompanyEntity> selectTable;
-    @FXML
-    private Button selectButton;
-    @FXML
-    private Button createButton;
-    @FXML
-    private Button EditButton;
-    @FXML
-    private Button DeleteButton;
-    @FXML
-    private TableColumn<CompanyEntity, Long> codeColumn;
-    @FXML
-    private TableColumn<CompanyEntity, String> vatColumn;
-    @FXML
-    private TableColumn<CompanyEntity, String> nameColumn;
-    @FXML
-    private CbbSearchableTextField searchTextField;
-    @FXML
-    private StackPane mainStackPane;
+  @FXML private TableView<CompanyEntity> selectTable;
+  @FXML private Button selectButton;
+  @FXML private Button createButton;
+  @FXML private Button EditButton;
+  @FXML private Button DeleteButton;
+  @FXML private TableColumn<CompanyEntity, Long> codeColumn;
+  @FXML private TableColumn<CompanyEntity, String> vatColumn;
+  @FXML private TableColumn<CompanyEntity, String> nameColumn;
+  @FXML private CbbSearchableTextField searchTextField;
+  @FXML private StackPane mainStackPane;
 
-    ObservableList<CompanyEntity> companyList;
-    FilteredList<CompanyEntity> filteredList;
-    private MaskerPane masker = new MaskerPane();
+  ObservableList<CompanyEntity> companyList;
+  FilteredList<CompanyEntity> filteredList;
+  private MaskerPane masker = new MaskerPane();
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        selectTable.setPlaceholder(new Label("Δεν υπάρχουν εταιρίες"));
-        mainStackPane.getChildren().add(masker);
-        masker.setVisible(true);
+  /** Initializes the controller class. */
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+    // disable buttons if nothing is selected
+    DeleteButton.disableProperty()
+        .bind(Bindings.isEmpty(selectTable.getSelectionModel().getSelectedItems()));
+    EditButton.disableProperty()
+        .bind(Bindings.isEmpty(selectTable.getSelectionModel().getSelectedItems()));
+    selectButton
+        .disableProperty()
+        .bind(Bindings.isEmpty(selectTable.getSelectionModel().getSelectedItems()));
+    selectTable.setPlaceholder(new Label("Δεν υπάρχουν εταιρίες"));
+    mainStackPane.getChildren().add(masker);
+    masker.setVisible(true);
 
-        codeColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
-        vatColumn.setCellValueFactory(new PropertyValueFactory<>("vatNumber"));
-        nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
-        searchTextField
-                .textProperty()
-                .addListener(
-                        (observable, oldValue, newValue) -> {
-                            filteredList.setPredicate(
-                                    t -> {
-                                        if (newValue == null || newValue.isEmpty()) {
-                                            return true;
-                                        }
-                                        String lowerCaseFilter = newValue.toLowerCase();
-                                        if (t.getName().toLowerCase().contains(lowerCaseFilter)) {
-                                            return true; // Filter matches name.
-                                        }
-                                        if (t.getVatNumber().toLowerCase().contains(lowerCaseFilter)) {
-                                            return true; // Filter matches vat
-                                        }
+    codeColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+    vatColumn.setCellValueFactory(new PropertyValueFactory<>("vatNumber"));
+    nameColumn.setCellValueFactory(new PropertyValueFactory<>("name"));
+    searchTextField
+        .textProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              filteredList.setPredicate(
+                  t -> {
+                    if (newValue == null || newValue.isEmpty()) {
+                      return true;
+                    }
+                    String lowerCaseFilter = newValue.toLowerCase();
+                    if (t.getName().toLowerCase().contains(lowerCaseFilter)) {
+                      return true; // Filter matches name.
+                    }
+                    if (t.getVatNumber().toLowerCase().contains(lowerCaseFilter)) {
+                      return true; // Filter matches vat
+                    }
 
-                                        if (Long.toString(t.getId()).contains(lowerCaseFilter)) {
-                                            return true; // Filter matches id
-                                        }
-                                        return false;
-                                    });
-                        });
-        loadService();
-    }
+                    if (Long.toString(t.getId()).contains(lowerCaseFilter)) {
+                      return true; // Filter matches id
+                    }
+                    return false;
+                  });
+            });
+    loadService();
+  }
 
-    @FXML
-    private void onSelect(ActionEvent event) {
-    }
+  @FXML
+  private void onSelect(ActionEvent event) {}
 
-    @FXML
-    private void onCreate(ActionEvent event) {
-    }
+  @FXML
+  private void onCreate(ActionEvent event) {}
 
-    @FXML
-    private void onEdit(ActionEvent event) {
-    }
+  @FXML
+  private void onEdit(ActionEvent event) {}
 
-    @FXML
-    private void onDelete(ActionEvent event) {
-    }
+  @FXML
+  private void onDelete(ActionEvent event) {}
 
-    private void loadService() {
-        masker.setVisible(true);
-        masker.setText("Φόρτωση εταιριών\nΠαρακαλώ περιμένετε");
-        Task<String> service
-                = new Task<String>() {
-            @Override
-            protected String call() throws Exception {
-                selectTable.setItems(null);
-                loadTable(CompanyQueries.getCompanies());
-                return null;
-            }
+  private void loadService() {
+    masker.setVisible(true);
+    masker.setText("Φόρτωση εταιριών\nΠαρακαλώ περιμένετε");
+    Task<String> service =
+        new Task<String>() {
+          @Override
+          protected String call() throws Exception {
+            selectTable.setItems(null);
+            loadTable(CompanyQueries.getCompanies());
+            return null;
+          }
         };
-        service.setOnSucceeded(
-                new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent t) {
-                masker.setVisible(false);
-            }
+    service.setOnSucceeded(
+        new EventHandler<WorkerStateEvent>() {
+          @Override
+          public void handle(WorkerStateEvent t) {
+            masker.setVisible(false);
+          }
         });
-        Thread thread = new Thread(service);
-        thread.start();
-    }
+    Thread thread = new Thread(service);
+    thread.start();
+  }
 
-    private void loadTable(List<CompanyEntity> b) {
+  private void loadTable(List<CompanyEntity> b) {
 
-        companyList = FXCollections.observableArrayList(b);
-        filteredList = new FilteredList<>(companyList, p -> true);
-        SortedList<CompanyEntity> sortedData = new SortedList<>(filteredList);
+    companyList = FXCollections.observableArrayList(b);
+    filteredList = new FilteredList<>(companyList, p -> true);
+    SortedList<CompanyEntity> sortedData = new SortedList<>(filteredList);
 
-        sortedData.comparatorProperty().bind(selectTable.comparatorProperty());
-        selectTable.setItems(sortedData);
-    }
+    sortedData.comparatorProperty().bind(selectTable.comparatorProperty());
+    selectTable.setItems(sortedData);
+  }
 }
