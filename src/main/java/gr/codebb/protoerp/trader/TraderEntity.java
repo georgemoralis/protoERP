6af45b6d717c36db29a57b8a361e0a15b1baa19e
georgemoralis <giorgosmrls@gmail.com>
2021-03-19 14:47:44 +0200

    Δημιουργία entity για συναλλασόμενους

--- src/main/java/gr/codebb/protoerp/trader/TraderEntity.java
+++ src/main/java/gr/codebb/protoerp/trader/TraderEntity.java
@@ -4,6 +4,71 @@
  * ProtoERP - Open source invocing program
  * info@codebb.gr
  */
+/*
+ * changelog
+ * =========
+ * 19/03/2021 (gmoralis) - Αρχικό βασισμένο στο companyEntity
+ */
 package gr.codebb.protoerp.trader;
 
-public class TraderEntity {}
+import gr.codebb.protoerp.settings.company.CompanyEntity;
+import gr.codebb.protoerp.settings.doy.DoyEntity;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "traders")
+public class TraderEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Getter
+  @Setter
+  private Long id;
+
+  @Getter @Setter private String name;
+  @Getter @Setter private String registeredName;
+  @Getter @Setter private String job;
+  @Getter @Setter private String vatNumber;
+
+  @ManyToOne
+  @JoinColumn(name = "doy_id")
+  @Getter
+  @Setter
+  private DoyEntity doy;
+
+  @Getter @Setter private String email;
+  @Getter @Setter private String mobilePhone;
+  @Getter @Setter private Boolean active;
+
+  @ManyToOne
+  @JoinColumn(name = "company_id")
+  @Getter
+  @Setter
+  private CompanyEntity company;
+
+  @OneToMany(mappedBy = "trader", cascade = CascadeType.ALL, orphanRemoval = true)
+  @Getter
+  private List<TraderPlantsEntity> plantLines = new ArrayList<>();
+
+  public void addPlantLine(TraderPlantsEntity line) {
+    plantLines.add(line);
+    line.setTrader(this);
+  }
+
+  public void removePlantLine(TraderPlantsEntity line) {
+    plantLines.remove(line);
+    line.setTrader(null);
+  }
+}
---
+++ src/main/java/gr/codebb/protoerp/trader/TraderPlantsEntity.java
@@ -0,0 +1,68 @@
+/*
+ * copyright 2013-2021
+ * codebb.gr
+ * ProtoERP - Open source invocing program
+ * info@codebb.gr
+ */
+/*
+ * changelog
+ * =========
+ * 19/03/2021 (gmoralis) - Αρχικό βασισμένο στο PlantsEntity
+ */
+
+package gr.codebb.protoerp.trader;
+
+import gr.codebb.lib.crud.intf.Displayable;
+import gr.codebb.protoerp.settings.countries.CountriesEntity;
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "trader_plans")
+public class TraderPlantsEntity implements Serializable, Displayable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Getter
+  @Setter
+  private Long id;
+
+  @Getter @Setter private Integer code;
+  @Getter @Setter private String description;
+  /** Στοιχεία εγκατάστασης */
+  @Getter @Setter private String address;
+
+  @Getter @Setter private String area;
+  @Getter @Setter private String tk;
+  @Getter @Setter private String city;
+  @Getter @Setter private String phone;
+  @Getter @Setter private String fax;
+  /** References */
+  @ManyToOne
+  @JoinColumn(name = "country_id")
+  @Getter
+  @Setter
+  private CountriesEntity country;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "trader_id")
+  @Getter
+  @Setter
+  private TraderEntity trader;
+
+  @Getter @Setter private Boolean active;
+
+  @Override
+  public String getComboDisplayValue() {
+    return getCode() + " - " + getDescription();
+  }
+}
