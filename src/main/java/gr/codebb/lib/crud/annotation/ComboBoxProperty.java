f02012fba8109cd62bfbb609283ff976a8fddaf0
georgemoralis <giorgosmrls@gmail.com>
2021-04-01 00:18:00 +0300

    wip crud ComboBoxProperty

---
+++ src/main/java/gr/codebb/lib/crud/annotation/ComboBoxProperty.java
@@ -0,0 +1,20 @@
+/*
+ * copyright 2013-2021
+ * codebb.gr
+ * ProtoERP - Open source invocing program
+ * info@codebb.gr
+ */
+package gr.codebb.lib.crud.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD})
+public @interface ComboBoxProperty {
+
+  /* @return value default is comboBox */
+  public String value() default "comboBox";
+}
